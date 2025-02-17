e7d00ab2b5b8841531a734874e7376f5bf187fef
Matthew Xie <mattx180@gmail.com>
2025-02-17 02:07:15 -0500

    tweak hyperparameters and add wandb

--- alt.py
+++ alt.py
@@ -4,28 +4,40 @@ from collections import deque
 import numpy as np
 import random
 from alt_dqn import AltDeepQNetwork
-from envs import alt_tetris
-from torch.utils.tensorboard import SummaryWriter
+from envs.alt_tetris import TetrisEnv
+import wandb
 from tqdm import tqdm
 
 GAMMA = 0.99
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 REPLAY_SIZE = 300_000
 MIN_REPLAY_SIZE = 50_000
 EPSILON_START = 1.00
 EPSILON_END = 1e-3
 EPSILON_DECAY = 20_000
-EPSILON_DECAY_RATE = 0.998
+EPSILON_DECAY_RATE = 0.999
 LEARNING_RATE = 1e-3
 LEARNING_RATE_DECAY = 0.9
 LEARNING_RATE_DECAY_FREQ = 500
 NUM_ACTIONS = 40
 NUM_EPOCHS = 3_000
-NUM_STEPS = 50_000_000
-MAX_EPOCH_STEPS = 8000
+NUM_STEPS = 10_000_000
+MAX_EPOCH_STEPS = 2000
 TAU = 0.005
 TARGET_UPDATE_FREQ = 10_000
 SAVE_FREQ = 10_000
+WANDB = True
+
+if WANDB:
+    wandb.init(
+        project="Tetris CNN",
+        config={
+            "learning_rate": LEARNING_RATE,
+            "epochs": NUM_STEPS,
+            "batch_size": BATCH_SIZE,
+            "replay_size": REPLAY_SIZE,
+        },
+    )
 
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
@@ -33,7 +45,7 @@ torch.backends.cudnn.allow_tf32 = True
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 # writer = SummaryWriter()
 
-env = alt_tetris.TetrisEnv()
+env = TetrisEnv()
 
 replay_memory = deque(maxlen=REPLAY_SIZE)
 reward_memory = deque(maxlen=10)
@@ -75,9 +87,11 @@ def calculate_priority():
     rewards = torch.as_tensor(
         np.array([m[2] for m in replay_memory]), dtype=torch.float32
     ).to(device)
-    new_states = torch.as_tensor(
-        np.array([m[4] for m in replay_memory]), dtype=torch.float32
-    ).to(device)
+    new_states = (
+        torch.as_tensor(np.array([m[4] for m in replay_memory]), dtype=torch.float32)
+        .squeeze(1)
+        .to(device)
+    )
 
     target_net.eval()
     policy_net.eval()
@@ -105,7 +119,7 @@ for _ in range(MIN_REPLAY_SIZE):
         action = np.random.randint(0, 40)
 
     new_state, reward, done, info = env.step(action % 10, int(action / 10), probe=False)
-    transition = (state, action, reward, done, new_state, valid_moves_mask)
+    transition = (state, action, reward, done, new_state, valid_moves_mask.to("cpu"))
     replay_memory.append(transition)
 
     state = new_state
@@ -123,9 +137,9 @@ state = env.reset()
 progress_bar = tqdm(range(NUM_STEPS), desc="Training Progress")
 
 # training loop
-for i in tqdm(range(NUM_STEPS)):
+for i in tqdm(range(NUM_STEPS), position=0, leave=True):
     rand_sample = random.random()
-    valid_moves_mask = torch.tensor(env.get_invalid_moves()).unsqueeze(0)
+    valid_moves_mask = torch.tensor(env.get_invalid_moves()).unsqueeze(0).to(device)
 
     if rand_sample <= epsilon:
         action = np.random.randint(0, 40)
@@ -139,7 +153,7 @@ for i in tqdm(range(NUM_STEPS)):
         policy_net.eval()
         with torch.no_grad():
             q_values = policy_net(input)
-            q_values[valid_moves_mask] = -float("inf")
+            q_values = torch.where(valid_moves_mask, q_values, -1e9)
         policy_net.train()
 
         action = torch.argmax(q_values).item()
@@ -147,7 +161,7 @@ for i in tqdm(range(NUM_STEPS)):
     new_state, reward, done, lines_cleared = env.step(
         action % 10, int(action / 10), probe=False
     )
-    transition = (state, action, reward, done, new_state, valid_moves_mask)
+    transition = (state, action, reward, done, new_state, valid_moves_mask.to("cpu"))
     replay_memory.append(transition)
 
     state = new_state
@@ -170,21 +184,15 @@ for i in tqdm(range(NUM_STEPS)):
     states = torch.as_tensor(
         np.array([t[0] for t in transitions]), dtype=torch.float32
     ).to(device)
-    actions = (
-        torch.as_tensor(np.asarray([t[1] for t in transitions]), dtype=torch.int64)
-        .unsqueeze(-1)
-        .to(device)
-    )
-    rewards = (
-        torch.as_tensor(np.asarray([t[2] for t in transitions]), dtype=torch.float32)
-        .unsqueeze(-1)
-        .to(device)
-    )
-    dones = (
-        torch.as_tensor(np.asarray([t[3] for t in transitions]), dtype=torch.float32)
-        .unsqueeze(-1)
-        .to(device)
-    )
+    actions = torch.as_tensor(
+        np.asarray([t[1] for t in transitions]), dtype=torch.int64
+    ).to(device)
+    rewards = torch.as_tensor(
+        np.asarray([t[2] for t in transitions]), dtype=torch.float32
+    ).to(device)
+    dones = torch.as_tensor(
+        np.asarray([t[3] for t in transitions]), dtype=torch.float32
+    ).to(device)
     new_states = torch.as_tensor(
         np.array([t[4] for t in transitions]), dtype=torch.float32
     ).to(device)
@@ -197,48 +205,41 @@ for i in tqdm(range(NUM_STEPS)):
     # calculate targets and q-values
     policy_net.eval()
     target_net.eval()
+
     q_values = policy_net(states)
-    q_values[valid_moves] = -float("inf")
     with torch.no_grad():
         target_q_values = target_net(new_states)
+
+    q_values = torch.where(valid_moves, q_values, -1e9)
+    # target_q_values = torch.where(valid_moves, target_q_values, -1e9)
+
     policy_net.train()
     target_net.train()
+
+    target_q_values = target_q_values.max(dim=1)[0]
+    q_values = q_values.gather(1, actions.unsqueeze(-1)).flatten()
+
     targets = rewards + (GAMMA * (1 - dones) * (target_q_values))
 
     # gradient descent
     loss = criterion(q_values, targets)
     optimizer.zero_grad()
     loss.backward()
-    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    nn.utils.clip_grad_norm_(policy_net.parameters(), max_norm=1.0)
     optimizer.step()
     # scheduler.step(loss)
 
     # logs
-    # print()
-    # print("Epoch: ", epoch)
-    # print("Reward: ", episode_reward)
-    # print("Lines cleared: ", episode_lines_cleared)
-    # print("Epsilon: ", epsilon)
-    # print("LR: ", get_lr(optimizer))
-    # print("Loss: ", loss.item())
-
-    # writer.add_scalar("Reward", episode_reward, epoch)
-    # writer.add_scalar("Lines Cleared", episode_lines_cleared, epoch)
-    # writer.add_scalar("Epsilon", epsilon, epoch)
-    # writer.add_scalar("Learning Rate", get_lr(optimizer), epoch)
-    # writer.add_scalar("Loss", loss.item(), epoch)
-    # writer.flush()
-
-    progress_bar.set_postfix(
-        {
-            "Episode Reward": episode_reward,
-            "Episode Lines Cleared": episode_lines_cleared,
-            "Learning Rate": get_lr(optimizer),
-            "Loss": loss.item(),
-            "Epsilon": round(epsilon, 3),
-            "Steps": step_count,
-        }
-    )
+    if WANDB:
+        wandb.log(
+            {
+                "loss": loss.item(),
+                "Episode Reward": episode_reward,
+                "Episode Lines Cleared": episode_lines_cleared,
+                "Learning Rate": get_lr(optimizer),
+                "Epsilon": round(epsilon, 3),
+            }
+        )
 
     epoch_step = 0
     episode_reward = 0.0
@@ -268,6 +269,9 @@ for i in tqdm(range(NUM_STEPS)):
         torch.save(policy_net.state_dict(), "optimal_policy_weights.pth")
         torch.save(target_net.state_dict(), "optimal_target_weights.pth")
         reward_avg = avg
+
+if WANDB:
+    wandb.finish()
 
 torch.save(policy_net.state_dict(), "policy_weights.pth")
 torch.save(target_net.state_dict(), "target_weights.pth")
